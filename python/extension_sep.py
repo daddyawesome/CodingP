d7ce4c44f9a1f1f859f0fdebd5201e42a06df425
daddyawesome <sablay296@gmail.com>
2019-11-08 20:10:44 +0000

    python exercise about filename extension separation

---
+++ python/extension_sep.py
@@ -0,0 +1,8 @@
+# Write a Python program to accept a filename from the user and print the extension of that. Go to the editor
+# Sample filename : abc.java
+# Output : java
+
+fName = input("please enter the filename:")
+ext = fName.split(".")
+
+print(ext[1])
