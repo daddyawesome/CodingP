a55857aef2677836886e4572d87eea9021d0c03b
daddyawesome <sablay296@gmail.com>
2019-11-11 20:42:15 +0000

    String lecture on Python Data Structures

--- py4e/lec6.py
+++ py4e/lec6.py
@@ -7,3 +7,18 @@ for letter in word:
     print(letter)
 
 print(count)
+
+#Print out every call on reference
+
+print("\n From here we write using reference")
+print(word[0])
+print(word[2])
+print(word[5])
+
+#Slicing Strings
+s = "Monty Python"
+ 
+print("\n slicing strings")
+print(s[0:4])
+print(s[6:7])
+print(s[6:20])
