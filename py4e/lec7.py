9f357bb9749cf871d416a70a4404f15c76256d04
daddyawesome <sablay296@gmail.com>
2019-11-12 19:03:12 +0000

    adding lecture 7 on Python Data Structures

--- py4e/lec7.py
+++ py4e/lec7.py
@@ -1,3 +1,13 @@
 #fhand is just a file handler
-fhand = open('mbox.txt')
+fhand = open('mbox-short.txt')
+
+#this will not print the text inside the file
 print(fhand)
+
+
+count = 0
+for line in fhand:
+    if line.startswith('From:'):
+        print(line)
+        count = count + 1
+print(count)
