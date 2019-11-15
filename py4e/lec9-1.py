a40f3164200164c048e8f45826d48b3738d51d4c
daddyawesome <sablay296@gmail.com>
2019-11-15 22:04:27 +0000

    use dictionarie to count the max number of words

--- py4e/lec9-1.py
+++ py4e/lec9-1.py
@@ -1,1 +1,23 @@
 #This the most important lecture notes
+#This will count the most number of words in the file
+#
+name = input('Enter File:')
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    line = line.rstrip()
+    words = line.split()
+    
+    for wds in words:
+        counts[wds] = counts.get(wds, 0) +1
+
+maxword = None
+maxnum = None
+
+for key,val in counts.items():
+    if maxnum is None or val > maxnum:
+        maxword = key
+        maxnum = val
+        
+print(maxword,maxnum)
