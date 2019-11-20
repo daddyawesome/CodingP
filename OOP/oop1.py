5958320023169b0e4f2271c8859cafdf05aceccc
daddyawesome <sablay296@gmail.com>
2019-11-20 20:22:04 +0000

    class variable OOP

--- OOP/oop1.py
+++ OOP/oop1.py
@@ -8,5 +8,12 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
     
+    def apply_raise(self):
+        self.pay = int(self.pay * 1.04)
+        
 emp_1 = Employee('Mark','Russel', 50000)
 emp_2 = Employee('Test', 'User', 40000)
+
+print(emp_1.pay)
+print(emp_1.apply_raise())
+print(emp_1.pay)
