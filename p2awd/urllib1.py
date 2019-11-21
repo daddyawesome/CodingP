7895720906a6bf6738ee1668ec143f3b959379eb
daddyawesome <sablay296@gmail.com>
2019-11-21 20:16:47 +0000

    make use of URLLIB1 instead of socket

--- p2awd/urllib1.py
+++ p2awd/urllib1.py
@@ -6,6 +6,6 @@ count = dict()
 for line in fhand:
     words = line.decode().split()
     for word in words:
-        counts[word] = counts.get(word, 0) + 1
+        count[word] = count.get(word, 0) + 1
         
 print(count)
