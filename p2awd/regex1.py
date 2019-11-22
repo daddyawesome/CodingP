79dd14df7e68796cbf64377a6cd7690d2313bb37
daddyawesome <sablay296@gmail.com>
2019-11-22 17:30:35 +0000

    regex code

--- p2awd/regex1.py
+++ p2awd/regex1.py
@@ -1,3 +1,4 @@
+Learn more or give us feedback
 import re
 hand = open("mbox-short.txt")
 numlist = list()
@@ -5,12 +6,9 @@ numlist = list()
 for line in hand:
     line = line.rstrip()
     # the line below is to search for the line that strats with X-DSPAM-Confidence then the number after :
-    stuff = re.findall('href=".+"', line)
-   
-    if len(stuff) < 1: continue
-    print(stuff)
- 
-    #num = float(stuff[0])
-    #numlist.append(num)
-print(stuff)
-#print("Maximum:",max(numlist))
+    stuff = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line)
+    if len(stuff) != 1: continue
+    num = float(stuff[0])
+    numlist.append(num)
+print(numlist)
+print("Maximum:",max(numlist))
