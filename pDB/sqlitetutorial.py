18833c1c56259417672a1cc5f896c83879de488b
daddyawesome <sablay296@gmail.com>
2019-12-08 06:24:44 +0000

    inserting to sql

--- pDB/sqlitetutorial.py
+++ pDB/sqlitetutorial.py
@@ -8,6 +8,7 @@ cur = con.cursor() # instantiate a cursor obj
 cur.execute('DROP TABLE IF EXISTS customers')
 cur.execute('DROP TABLE IF EXISTS products')
 cur.execute('DROP TABLE IF EXISTS orders')
+cur.execute('DROP TABLE IF EXISTS lineitems')
 customers_sql = """
  CREATE TABLE customers (
      id integer PRIMARY KEY,
@@ -29,6 +30,17 @@ orders_sql = """
      customer_id integer,
      FOREIGN KEY (customer_id) REFERENCES customers (id))"""
 cur.execute(orders_sql)
+
+lineitems_sql = """
+ CREATE TABLE lineitems (
+     id integer PRIMARY KEY,
+     quantity integer NOT NULL,
+     total real NOT NULL,
+     product_id integer,
+     order_id integer,
+     FOREIGN KEY (product_id) REFERENCES products (id),
+     FOREIGN KEY (order_id) REFERENCES orders (id))"""
+cur.execute(lineitems_sql)
 
 #Loading the Data
 
@@ -68,3 +80,28 @@ def create_lineitem(con, order_id, product_id, qty, total):
     cur = con.cursor()
     cur.execute(sql, (order_id, product_id, qty, total))
     return cur.lastrowid
+
+#Inserting data
+#
+try:
+    codd_id = create_customer(con, 'Edgar', 'Codd')
+    codd_order = create_order(con, codd_id, '1969-01-12')
+    codd_li = create_lineitem(con, codd_order, 4, 1, 16.99)
+    knuth_id = create_customer(con, 'Donald', 'Knuth')
+    knuth_order = create_order(con, knuth_id, '1967-07-03')
+    knuth_li1 = create_lineitem(con, knuth_order, 2, 1, 17.99)
+    knuth_li2 = create_lineitem(con, knuth_order, 3, 1, 11.99)
+
+    # commit the statements
+    con.commit()
+except:
+    # rollback all database actions since last commit
+    con.rollback()
+    raise RuntimeError("Uh oh, an error occurred ...")
+
+cur.execute("SELECT id, first_name, last_name FROM customers")
+results = cur.fetchall()
+for row in results:
+     print(row)
+
+cur.close()
