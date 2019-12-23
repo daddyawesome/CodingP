e1c92401a4937afa1af9499ba883ecadf3df8335
daddyawesome <sablay296@gmail.com>
2019-12-23 23:36:14 +0000

    chec

--- checkio/elementary/all_d_same.py
+++ checkio/elementary/all_d_same.py
@@ -1,15 +1,19 @@
 def all_the_same(numbers):
-        x =None
-    for n in numbers:
-        x = x+n    
-        if x is None or x == n: continue
-        else:
-            return False
+    if len(numbers) == 0:
+        return True
+    else:
+        nTemp = numbers[0]
+            
+        for n in numbers:
+            print(n)
+            print(nTemp)
+            if nTemp == n:continue
+            else:
+                return False
+        return True
 
-
-#all_the_same([1, 1, 1]) == True
-#all_the_same([1, 2, 1]) == False
-#all_the_same(['a', 'a', 'a']) == True
-#all_the_same([]) == True
-
+         
+print(all_the_same([1, 1, 1]))
 print(all_the_same([1, 2, 1]))
+print(all_the_same(['a', 'a', 'a']))
+print(all_the_same([]))
