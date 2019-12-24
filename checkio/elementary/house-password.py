044dfd68c817c42d8c695f0da6077035d152d9c1
daddyawesome <sablay296@gmail.com>
2019-12-24 20:02:22 +0000

    house password

---
+++ checkio/elementary/house-password.py
@@ -0,0 +1,26 @@
+import re
+def checkio(p: str) -> bool:
+    x = True
+
+    if (len(p)<10):
+        x=False
+    elif not re.search("[a-z]",p):
+        x=False
+    elif not re.search("[0-9]",p):
+        x=False
+    elif not re.search("[A-Z]",p):
+        x=False
+    else:
+        print("Valid Password")
+
+    return x    
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
