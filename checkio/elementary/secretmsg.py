71056865637b47c7af8871cb1ff05852cb425cb0
daddyawesome <sablay296@gmail.com>
2019-12-25 18:02:37 +0000

    secret message with other solution

--- checkio/elementary/secretmsg.py
+++ checkio/elementary/secretmsg.py
@@ -9,3 +9,8 @@ def find_message(text: str) -> str:
 if __name__ == '__main__':
     print('Example:')
     print(find_message("How are you? Eh, ok. Low or Lower? Ohhh."))
+
+   #other solution
+def find_message2(text: str) -> str:
+    """Find a secret message"""
+    return ''.join(filter(str.isupper, text))
