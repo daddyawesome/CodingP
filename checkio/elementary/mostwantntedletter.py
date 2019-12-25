8c725f0e040f9de474c47048ebc9f355c0fb828b
daddyawesome <sablay296@gmail.com>
2019-12-25 16:15:16 +0000

    most letters in checkio

--- checkio/elementary/mostwantntedletter.py
+++ checkio/elementary/mostwantntedletter.py
@@ -10,3 +10,17 @@ def checkio(words):
     print (dict)
     d3={v:k for k,v in dict.items()}
     return d3[max(d3)]
+string = input("Please Enter your Own String : ")
+alphabets = digits = special = 0
+
+for i in range(len(string)):
+    if(string[i].isalpha()):
+        alphabets = alphabets + 1
+    elif(string[i].isdigit()):
+        digits = digits + 1
+    else:
+        special = special + 1
+        
+print("\nTotal Number of Alphabets in this String :  ", alphabets)
+print("Total Number of Digits in this String :  ", digits)
+print("Total Number of Special Characters in this String :  ", special)
