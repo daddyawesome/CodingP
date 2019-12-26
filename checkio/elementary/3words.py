c1f2f63778265be98eb98dd0c3280bec68ea5f27
daddyawesome <sablay296@gmail.com>
2019-12-26 18:03:06 +0000

    3 words fix bug

--- checkio/elementary/3words.py
+++ checkio/elementary/3words.py
@@ -10,9 +10,11 @@ def checkio(words: str) -> bool:
     for w in x:
         if w.isalpha():
             cnt = cnt + 1
+            if cnt > 2:
+                return True
         else:
             cnt = 0
-    return (cnt > 2)
+    return False
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
@@ -24,4 +26,5 @@ if __name__ == '__main__':
     assert checkio("1 2 3 4") == False, "Digits"
     assert checkio("bla bla bla bla") == True, "Bla Bla"
     assert checkio("Hi") == False, "Hi"
+    assert checkio("one two 3 four five six 7 eight 9 ten eleven 12") == True, "count"
     print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
