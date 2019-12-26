adb783bb01374544a3ab8a6302714e18164801c5
daddyawesome <sablay296@gmail.com>
2019-12-26 15:02:19 +0000

    the simpler way to subtract max and min

--- checkio/elementary/max-min.py
+++ checkio/elementary/max-min.py
@@ -5,7 +5,7 @@ def checkio(*args):
         
         x = max1 - min1
     except:
-        x = ""
+        x = 0
     return x
 
 if __name__ == '__main__':
@@ -15,3 +15,7 @@ if __name__ == '__main__':
         
     print('Example:')
     print(checkio(1, 2, 3))
+
+#other solution
+def checkio2(*args):
+    return sorted(args)[-1] - sorted(args)[0] if len(args) > 1 else 0
