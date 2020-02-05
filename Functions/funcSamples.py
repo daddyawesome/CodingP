fd2cb088056acfa0ebd1099435c44cf610cff6ea
daddyawesome <sablay296@gmail.com>
2020-02-05 20:05:18 +0000

    study on functions

---
+++ Functions/funcSamples.py
@@ -0,0 +1,9 @@
+fam = [1.2, 3.24, 0.005]
+x=min(fam)      #min() is an example of function
+y=max(fam)      #max() is an example of function
+z= round(y,1)   #round() is an example of function
+print(x)
+print(y)
+print(z)
+
+help(round)
