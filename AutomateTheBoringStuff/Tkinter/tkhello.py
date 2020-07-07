e168b6a5044df9c2283cdd61b2f4131afb2b78e9
Mark Russel Miranda <markrussel.miranda@qualfon.com>
2020-07-07 16:19:52 -0400

    first step on GUI

---
+++ AutomateTheBoringStuff/Tkinter/tkhello.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+window = tk.Tk()
+label = tk.Label(
+    text="Hello, Tkinter",
+    foreground="white",  # Set the text color to white
+    background="black"  # Set the background color to black
+)
+label.pack()
+
+window.mainloop()
