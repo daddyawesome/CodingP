cb90b4216300b9dad436f47c86a3067c283a4d00
Daddy Awesome <sablay296@gmail.com>
2022-03-18 19:18:05 +0800

    tim games cont

--- pygame/timGame.py
+++ pygame/timGame.py
@@ -26,5 +26,18 @@ while running:
         if event.type == pygame.QUIT: #Check if the red button in the corner of the window is clicked
             running = False #End the game loop
 
+    keys = pygame.key.get_pressed()  # This will give us a dictonary where each key has a value of 1 or 0. Where 1 is pressed and 0 is not pressed.
+
+    if keys[pygame.K_LEFT]: # We can check if a key is pressed like this
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+    pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
+    pygame.display.update()
+    
 # Done! Time to quit.
 pygame.quit()
